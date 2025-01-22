82537a28e39d8d3a7c353c48b91ceb9b3b2ebb8f
anonymous298 <tackletalha@gmail.com>
2025-01-22 23:42:40 +0500

    changes

---
+++ simple_nn.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+training_data = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+testing_data = datasets.FashionMNIST(
+    root='data',
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+batch_size = 64
+
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(testing_data, batch_size=batch_size)
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
